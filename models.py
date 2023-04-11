7d194310517b820d2e4c2f13fb0a017bd620999f
piercephillips23 <129883018+piercephillips23@users.noreply.github.com>
2023-04-11 14:38:51 -0400

    Update models.py

--- models.py
+++ models.py
@@ -42,7 +42,7 @@ class GRU(nn.Module):
     
     
 class LSTM(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size, num_layers=2):
+    def __init__(self, input_size, hidden_size, num_layers, output_size):
         super(LSTM, self).__init__()
         
         self.hidden_size = hidden_size
