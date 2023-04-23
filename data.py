e5757f6eb4dec7f01a46a9e9078591546e2644eb
piercephillips23 <129883018+piercephillips23@users.noreply.github.com>
2023-04-23 19:56:11 -0400

    Update data.py

--- data.py
+++ data.py
@@ -3,8 +3,9 @@ from sklearn.preprocessing import MinMaxScaler
 import numpy as np
 import torch
 
+
 def pull_data(ticker, start="2000-01-01", end="2020-01-01", scale=True):
-  data = yf.download(ticker, "2000-01-01", "2020-01-01")
+  data = yf.download(ticker, start, end)
   price = data["Close"]
   
   if scale:
@@ -16,11 +17,10 @@ def pull_data(ticker, start="2000-01-01", end="2020-01-01", scale=True):
 def make_train_test(data, frac=0.7, bin_size=14):
   seqs = list()
   for idx in range(data.shape[0] - bin_size):
-      seqs.append(price[idx:idx+bin_size])
+      seqs.append(data[idx:idx+bin_size])
       
   seqs = np.array(seqs)
   train_n = int(np.floor(frac * seqs.shape[0]))
-  test_n = seqs.shape[0] - train_n
 
   train_x = torch.from_numpy(seqs[:train_n,:-1,:]).type(torch.Tensor)
   train_y = torch.from_numpy(seqs[:train_n,-1,:]).type(torch.Tensor)
@@ -28,3 +28,16 @@ def make_train_test(data, frac=0.7, bin_size=14):
   test_y = torch.from_numpy(seqs[train_n:,-1,:]).type(torch.Tensor)
   
   return train_x, train_y, test_x, test_y
+
+
+def testing(data, bin_size=14):
+  seqs = list()
+  for idx in range(data.shape[0] - bin_size):
+      seqs.append(data[idx:idx+bin_size])
+      
+  seqs = np.array(seqs)
+
+  x = torch.from_numpy(seqs[:,:-1,:]).type(torch.Tensor)
+  y = torch.from_numpy(seqs[:,-1,:]).type(torch.Tensor)
+  
+  return x, y
